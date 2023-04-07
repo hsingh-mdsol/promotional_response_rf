5d34ed854399feab83b1e267e0ce650816d53a8f
hartsingh <hartsingh@mdsol.com>
2023-04-07 13:05:42 -0400

    updated response curves to include min max scaled hill estimates

--- research/mmm_response_curves.py
+++ research/mmm_response_curves.py
@@ -49,9 +49,10 @@ class MMMResponseCurves(object):
             touches = self._scaling(seg_resp['touches'])
             y_axis = self._scaling(seg_resp[i])
             try:
-                #popt, pcov = curve_fit(self._response_curve_hill, seg_resp['touches'], seg_resp[i],
+                # popt, pcov = curve_fit(self._response_curve_hill, seg_resp['touches'], seg_resp[i],
                 #                       bounds=(0, [np.inf, np.inf, np.inf]))
-                popt, pcov = curve_fit(self._response_curve_hill, touches['scaled_vals'], y_axis['scaled_vals'],
+                popt, pcov = curve_fit(self._response_curve_hill, touches['scaled_vals'],
+                                       y_axis['scaled_vals'],
                                        bounds=(0, [np.inf, np.inf, np.inf]))
             except RuntimeError:
                 print("Error - curve_fit failed")
@@ -69,7 +70,8 @@ class MMMResponseCurves(object):
         fig_raw = self.plot(seg_resp, x='touches', y=seg_cols)
         return [seg_resp, opt_hill, fig_hill, fig_raw, scaler_x, scaler_y]
 
-    def _overall_response_aggr(self, df: pd.DataFrame, feature: str, max_freq: int, increment: int) -> []:
+    def _overall_response_aggr(self, df: pd.DataFrame, feature: str, max_freq: int,
+                               increment: int) -> []:
         """
         """
         # rf response curve
@@ -86,24 +88,32 @@ class MMMResponseCurves(object):
         touches = self._scaling(resp_final['touches'])
         y_axis = self._scaling(resp_final[feature])
         try:
-            #popt, pcov = curve_fit(self._response_curve_hill, resp_final['touches'], resp_final[feature],
+            # popt, pcov = curve_fit(self._response_curve_hill, resp_final['touches'], resp_final[feature],
             #                       bounds=(0, [np.inf, np.inf, np.inf]))
-            popt, pcov = curve_fit(self._response_curve_hill, touches['scaled_vals'], y_axis['scaled_vals'],
+            popt, pcov = curve_fit(self._response_curve_hill, touches['scaled_vals'],
+                                   y_axis['scaled_vals'],
                                    bounds=(0, [np.inf, np.inf, np.inf]))
         except RuntimeError:
             print("Error - curve_fit failed")
             popt = [0, 0, 0]
-        #resp_final[f"{feature}_hill_estimate"] = self._response_curve_hill(resp_final['touches'],
+        # resp_final[f"{feature}_hill_estimate"] = self._response_curve_hill(resp_final['touches'],
         #                                                                   popt[0], popt[1], popt[2])
-        hill_est = np.array(self._response_curve_hill(touches['scaled_vals'], popt[0], popt[1], popt[2])).reshape(-1, 1)
-        resp_final[f"{feature}_hill_estimate"] = y_axis['scaler'].inverse_transform(hill_est).flatten()
+        hill_est = np.array(
+            self._response_curve_hill(touches['scaled_vals'], popt[0], popt[1], popt[2])).reshape(
+            -1, 1)
+        resp_final[f"{feature}_hill_estimate"] = y_axis['scaler'].inverse_transform(
+            hill_est).flatten()
         resp_final["touches_scaled"] = touches['scaled_vals']
+        resp_final[f"{feature}_hill_estimate_minmax"] = resp_final[f"{feature}_hill_estimate"] / \
+                                                        (max(resp_final[f"{feature}_hill_estimate"])
+                                                         - min(resp_final[f"{feature}_hill_estimate"]))
         # plot final curve
         fig_raw = self.plot(resp_final, x='touches', y=[f"{feature}_hill_estimate"])
         fig_hill = self.plot(resp_final, x='touches', y=[feature])
         return [resp_final, popt, fig_hill, fig_raw, touches['scaler'], y_axis['scaler']]
 
-    def responses(self, curve_type: str, model, x: pd.DataFrame, feature: str, max_freq: int, increment: int,
+    def responses(self, curve_type: str, model, x: pd.DataFrame, feature: str, max_freq: int,
+                  increment: int,
                   segment: str, date: str, lag_channels=[]) -> dict:
         """
 
@@ -118,23 +128,29 @@ class MMMResponseCurves(object):
             df_sim[[feature] + lag_channels] = i
             # re adstock if applied
             if 'adstock' in feature:
-                df_sim[feature] = transform.segment_adstock(df_sim, feature, segment, float(feature.split('_')[-1]))
-            df_preds[f"{feature}_preds_{i}"] = model.predict(df_sim.drop([segment, date], errors='ignore', axis=1))
+                df_sim[feature] = transform.segment_adstock(df_sim, feature, segment,
+                                                            float(feature.split('_')[-1]))
+            df_preds[f"{feature}_preds_{i}"] = model.predict(
+                df_sim.drop([segment, date], errors='ignore', axis=1))
         # aggregate predictions
         if curve_type == 'segment':
             df_aggr = df_preds.drop([date], axis=1).groupby(segment).mean().reset_index()
-            resp_final, popt, fig_hill, fig_raw, scaler_x, scaler_y = self._segment_response_aggr(df_aggr, feature,
-                                                                                                  max_freq, increment,
-                                                                                                  segment)
+            resp_final, popt, fig_hill, fig_raw, scaler_x, scaler_y = self._segment_response_aggr(
+                df_aggr, feature,
+                max_freq, increment,
+                segment)
         elif curve_type == 'overall':
             # df_aggr = df_preds.drop([segment], axis=1).groupby(date).sum().reset_index()
-            resp_final, popt, fig_hill, fig_raw, scaler_x, scaler_y = self._overall_response_aggr(df_preds, feature,
-                                                                                                  max_freq, increment)
-        resp_dict = {'resp_df': resp_final, 'optimal_hill': popt, 'fig_hill': fig_hill, 'fig_raw': fig_raw,
+            resp_final, popt, fig_hill, fig_raw, scaler_x, scaler_y = self._overall_response_aggr(
+                df_preds, feature,
+                max_freq, increment)
+        resp_dict = {'resp_df': resp_final, 'optimal_hill': popt, 'fig_hill': fig_hill,
+                     'fig_raw': fig_raw,
                      'scaler_x': scaler_x, 'scaler_y': scaler_y}
         return resp_dict
 
-    def responses_segment(self, model, x: pd.DataFrame, feature: str, max_freq: int, increment: int, segments: [],
+    def responses_segment(self, model, x: pd.DataFrame, feature: str, max_freq: int, increment: int,
+                          segments: [],
                           segment: str, lag_channels=[]) -> dict:
         """
         """
@@ -149,7 +165,8 @@ class MMMResponseCurves(object):
                 df_sim[[feature] + lag_channels] = i
                 # re adstock if applied
                 if 'adstock' in feature:
-                    df_sim[feature] = transform.segment_adstock(df_sim, feature, segment, float(feature.split('_')[-1]))
+                    df_sim[feature] = transform.segment_adstock(df_sim, feature, segment,
+                                                                float(feature.split('_')[-1]))
                 pred.append(np.mean(model.predict(df_sim.drop([segment], errors='ignore', axis=1))))
             resp_final = pd.concat([resp_final, pd.DataFrame({f"{s}_{feature}": pred})], axis=1)
         # aggregate predictions
@@ -162,20 +179,24 @@ class MMMResponseCurves(object):
             touches = self._scaling(resp_final['touches'])
             y_axis = self._scaling(resp_final[i])
             try:
-                #popt, pcov = curve_fit(self._response_curve_hill, resp_final['touches'], resp_final[i],
+                # popt, pcov = curve_fit(self._response_curve_hill, resp_final['touches'], resp_final[i],
                 #                       bounds=(0, [np.inf, np.inf, np.inf]))
-                popt, pcov = curve_fit(self._response_curve_hill, touches['scaled_vals'], y_axis['scaled_vals'],
+                popt, pcov = curve_fit(self._response_curve_hill, touches['scaled_vals'],
+                                       y_axis['scaled_vals'],
                                        bounds=(0, [np.inf, np.inf, np.inf]))
             except RuntimeError:
                 print("Error - curve_fit failed")
                 popt = [0, 0, 0]
             hill_est = np.array(self._response_curve_hill(touches['scaled_vals'],
                                                           popt[0], popt[1], popt[2])).reshape(-1, 1)
-            resp_final[f"{i}_hill_estimate"] = y_axis['scaler'].inverse_transform(hill_est).flatten()
+            resp_final[f"{i}_hill_estimate"] = y_axis['scaler'].inverse_transform(
+                hill_est).flatten()
             opt_hill.update({i: popt})
         # plot final curve
-        fig_hill = self.plot(resp_final, x='touches', y=[x for x in resp_final.columns if "hill" in x])
+        fig_hill = self.plot(resp_final, x='touches',
+                             y=[x for x in resp_final.columns if "hill" in x])
         fig_raw = self.plot(resp_final, x='touches', y=raw_pred_cols)
         # final out
-        resp_dict = {'resp_df': resp_final, 'optimal_hill': opt_hill, 'fig_hill': fig_hill, 'fig_raw': fig_raw}
+        resp_dict = {'resp_df': resp_final, 'optimal_hill': opt_hill, 'fig_hill': fig_hill,
+                     'fig_raw': fig_raw}
         return resp_dict
