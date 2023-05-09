e08202d1e974a811fe14ee36f5a490830efa3939
hartsingh <hartsingh@mdsol.com>
2023-05-09 16:39:48 -0400

    combined hcp, time and segment, time preproccessing into one module

--- research/mmm_preprocessing.py
+++ research/mmm_preprocessing.py
@@ -6,12 +6,22 @@ import numpy as np
 
 class MMMPreprocessing:
 
-    def balance(self, df: pd.DataFrame, group_by: str, date: str, segments: [] = None) -> \
-            pd.DataFrame:
+    def remove(self, df: pd.DataFrame, group_by: str, dv: str, channels: []) -> pd.DataFrame:
+        """
+        """
+        df_g = df.groupby(group_by).max().reset_index()
+        df_g['drop'] = np.where(df_g[[dv] + channels].sum(axis=1) == 0, 1, 0)
+        drop_hcps = df_g[df_g['drop'] == 1][group_by]
+        df_out = df[~df[group_by].isin(drop_hcps)]
+        return df_out
+
+    def balance(self, df: pd.DataFrame, group_by: str, date: str, seg_cols=None) -> pd.DataFrame:
         """
         Balances data frame so each segment has the same number of months. Missing months will get
         filled with zeros
         """
+        if seg_cols is None:
+            seg_cols = []
         # get range of dates
         dates = pd.period_range(start=min(df[date]), end=max(df[date]), freq='M').tolist()
         # get list of unique npis
@@ -22,28 +32,79 @@ class MMMPreprocessing:
         # merge in original data
         df_out = df_out.merge(df, on=[group_by, date], how='left')
         # if static hcp level columns exist, fill the column with original values
-        if segments is not None:
-            for i in segments:
+        if seg_cols is not None:
+            for i in seg_cols:
                 df_out[i] = df_out.groupby([group_by])[i].bfill()
                 df_out[i] = df_out.groupby([group_by])[i].ffill()
         # replace nan with 0 since nan is the lack of occurrence of media for that time point
         df_out = df_out.fillna(0)
         return df_out
 
-    def one_hot(self, df: pd.DataFrame, segment: str) -> pd.DataFrame:
+    def one_hot(self, df: pd.DataFrame, seg_cols: []) -> pd.DataFrame:
         """
         Performance one-hot encoding on specified feature column. Necessary for some tree based
         models
         """
-        df_out = pd.concat([df, pd.get_dummies(df[[segment]])], axis=1)
+        df_out = df.copy()
+        for i in seg_cols:
+            df_out = pd.concat([df_out, pd.get_dummies(df_out[[i]])], axis=1)
         return df_out
 
-    def top_segment(self, df: pd.DataFrame, segment: str, group_by: str, top_n: int) -> pd.DataFrame:
+    def top_segment(self, df: pd.DataFrame, seg_cols: [], metric: str, top_n: int) -> pd.DataFrame:
         """
         """
         df_out = df.copy()
-        aggr = df_out.groupby(segment)[group_by].nunique().reset_index()
-        other_spec = aggr[aggr[group_by] <= top_n][segment]
-        df_out[segment] = np.where(df_out[segment].isin(other_spec), 'Other', df_out[segment])
+        for i in seg_cols:
+            aggr = df_out.groupby(i)[metric].nunique().reset_index()
+            other_spec = aggr[aggr[metric] <= top_n][i]
+            df_out[i] = np.where(df_out[i].isin(other_spec), 'Other', df_out[i])
         return df_out
+
+    def hcp_time_agg(self, df: pd.DataFrame, group_by: str, date: str, dv: str, channels: [],
+                     seg_cols=None) -> pd.DataFrame:
+        """
+        """
+        if seg_cols is None:
+            seg_cols = []
+        df_out = df[[group_by, date, dv] + channels + seg_cols]
+        df_out = self.balance(df_out, group_by, date, seg_cols)
+        if seg_cols is not None:
+            df_out = self.top_segment(df_out, seg_cols, group_by, 15)
+            df_out = self.one_hot(df_out, seg_cols)
+        return df_out
+
+    def segment_time_agg(self, df: pd.DataFrame, segment: str, date: str, dv: str, channels: []) \
+            -> pd.DataFrame:
+        """
+        """
+        df_out = df[[segment, date, dv] + channels].groupby([segment, date]).sum().reset_index().\
+            sort_values([segment, date])
+        df_out = self.top_segment(df_out, [segment], date, 15)
+        # ensure data is at segment, date level
+        df_out = df_out.groupby([segment, date]).sum().reset_index()
+        # balance
+        df_out = self.balance(df_out, segment, date)
+        # one hot encode segments
+        df_out = self.one_hot(df_out, [segment])
+        return df_out
+
+    def aggregation(self, df: pd.DataFrame, group_by: str, date: str, dv: str, channels: [],
+                    seg_cols=None) -> dict:
+        """
+        """
+        final_dct = {}
+        if seg_cols is None:
+            seg_cols = []
+        df_out = df.copy()
+        # hcp, time level aggregation
+        df_hcp_time = self.hcp_time_agg(df_out, group_by, date, dv, channels, seg_cols)
+        final_dct.update({'hcp_time': df_hcp_time})
+        # segment time
+        if seg_cols:
+            for i in seg_cols:
+                df_seg_time = self.segment_time_agg(df_out, i, date, dv, channels)
+                final_dct.update({f"segment_time_{i}": df_seg_time})
+        return final_dct
+
+
 
